a948deb3798a028f30d3811bc4e517f280b4f180
Rajib Mondal <rajibmondal4410@gmail.com>
2025-12-01 14:40:28 +0530

    Fix port for cloud deployment

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/test-direct", (req, res) => {
 });
 
 // Start Server on Port 5001
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
